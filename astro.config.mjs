43be29ea22c417c6a12824b2c5ba36ba260bf2a2
Hang <hangw0501@gmail.com>
2026-04-10 19:53:48 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
-import content from '@astrojs/content';
 import cloudflare from "@astrojs/cloudflare";
-
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   site: "https://example.com",
-  integrations: [content()],
-  integrations: [preact()]
+  integrations: [preact()],
 });
